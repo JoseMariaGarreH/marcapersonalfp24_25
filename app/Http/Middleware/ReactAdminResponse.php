52db5c1967455dc3097246109e1e074928fd3602
example <example@example.com>
2025-01-23 09:50:12 +0100

    Integrar el helper en el middleware

--- app/Http/Middleware/ReactAdminResponse.php
+++ app/Http/Middleware/ReactAdminResponse.php
@@ -11,32 +11,48 @@ class ReactAdminResponse
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function handle(Request $request, Closure $next): Response
     {
         $request->merge(['perPage' => 10]);
-        if($request->filled('_start')) {
-            if($request->filled('_end')) {
+        if ($request->filled('_start')) {
+            if ($request->filled('_end')) {
                 $request->merge(['perPage' => 1 + $request->_end - $request->_start]);
             }
-            $request->merge(['page' => intval($request->_start/ $request->perPage) + 1]);
+            $request->merge(['page' => intval($request->_start / $request->perPage) + 1]);
         }
 
         $response = $next($request);
-        if($request->routeIs('*.index')) {
-            abort_unless(property_exists($request->route()->controller, 'modelclass'), 500, "It must exists a modelclass property in the controller.");
+        if ($request->routeIs('*.index')) {
+            abort_unless(property_exists($request->route()->controller, 'modelclass'), 500, "It must exist a modelclass property in the controller.");
             $modelClassName = $request->route()->controller->modelclass;
-            $response->header('X-Total-Count',$modelClassName::count());
-        }
-        try {
-            if(is_callable([$response, 'getData'])) {
-                $responseData = $response->getData();
-                if(isset($responseData->data)) {
-                    $response->setData($responseData->data);
-                }
+
+            $query = $modelClassName::query();
+
+            $filterValue = $request->q;
+            $filterColumns = (new $modelClassName)->getFillable();
+
+            if ($filterValue) {
+                $query->where(function ($query) use ($filterValue, $filterColumns) {
+                    foreach ($filterColumns as $column) {
+                        $query->orWhere($column, 'like', '%' . $filterValue . '%');
+                    }
+                });
             }
-        } catch (\Throwable $th) { }
+
+            $query->orderBy($request->_sort ?? 'id', $request->_order ?? 'asc');
+
+            $results = $query->paginate($request->perPage);
+
+            $response->header('X-Total-Count', $results->total());
+
+            // Set the response data
+            $response->setData($results->items());
+        }
+
         return $response;
     }
 }
